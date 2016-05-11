8ae3a6161b7a686b5e01d6c6d6664204bef73cb6
Marcel Pociot <m.pociot@gmail.com>
2016-05-11 10:56:12 +0200

    Updated tocify

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,20 +1,20 @@
 ;
 /*
-Copyright 2016 Marcel Pociot
-Copyright 2008-2013 Concur Technologies, Inc.
+ Copyright 2016 Marcel Pociot
+ Copyright 2008-2013 Concur Technologies, Inc.
 
-Licensed under the Apache License, Version 2.0 (the "License"); you may
-not use this file except in compliance with the License. You may obtain
-a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not use this file except in compliance with the License. You may obtain
+ a copy of the License at
 
-  http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-License for the specific language governing permissions and limitations
-under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ License for the specific language governing permissions and limitations
+ under the License.
+ */
 (function (global) {
   'use strict';
   hljs.initHighlightingOnLoad();
@@ -150,8 +150,22 @@ under the License.
     }
   }
 
+  function slugify(text){
+    return text.toString().toLowerCase()
+        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+        .replace(/^-+/, '')             // Trim - from start of text
+        .replace(/-+$/, '');            // Trim - from end of text
+  }
+
   // if we click on a language tab, activate that language
   $(function() {
+
+    $('h1, h2').each(function(){
+      $(this).prop('id', slugify($(this).text()));
+    });
+
     $(".lang-selector a").on("click", function() {
       var language = $(this).data("language-name");
       pushURL(language);
@@ -162,19 +176,12 @@ under the License.
       activateLanguage(getLanguageFromQueryString());
     };
   });
-})(window);
-
-
-/**
- * TOC
- */
-(function (global) {
-  'use strict';
 
   var closeToc = function() {
     $(".tocify-wrapper").removeClass('open');
     $("#nav-button").removeClass('open');
   };
+
 
   var makeToc = function() {
     global.toc = $("#toc").tocify({
